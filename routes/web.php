cd7f6c86186fcd1105814fe3de8f805fd733c787
Jerome Jaglale <jerome.jaglale@gmail.com>
2024-01-30 12:01:22 +0000

    Update priorities when creating a new task as well

--- routes/web.php
+++ routes/web.php
@@ -5,49 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Task;
 use Illuminate\Support\Facades\Log;
 
-Route::get('/', function () {
-	$task_list = Task::orderBy('priority')->get();
-
-	$data = [];
-	$data['task_list'] = $task_list;
-
-    return view('task-list', $data);
-});
-
-Route::get('/create-task', function () {
-	$nb_tasks = Task::count();
-	$max_priority = $nb_tasks + 1;
-
-	$data = [];
-	$data['max_priority'] = $max_priority;
-
-    return view('create-task', $data);
-});
-
-Route::post('/create-task', function (Request $request) {
-	$task_data = $request->all();
-	$task = Task::create($task_data);
-
-	return redirect('/');
-});
-
-Route::get('/edit-task/{id}', function ($id) {
-	$task = Task::where('id', $id)->first();
-
-	if($task == null) {
-		abort(404);
-	}
-
-	$nb_tasks = Task::count();
-	$max_priority = $nb_tasks;
-
-	$data = [];
-	$data['task'] = $task;
-	$data['max_priority'] = $max_priority;
-
-    return view('edit-task', $data);
-});
-
 // if priority of task with $task_id has been updated
 // from $old_priority to $new_priority,
 // update priority of other tasks accordingly
@@ -73,6 +30,52 @@ function update_other_priorities($task_id, $old_priority, $new_priority) {
 		}
 	}
 }
+
+Route::get('/', function () {
+	$task_list = Task::orderBy('priority')->get();
+
+	$data = [];
+	$data['task_list'] = $task_list;
+
+    return view('task-list', $data);
+});
+
+Route::get('/create-task', function () {
+	$nb_tasks = Task::count();
+	$max_priority = $nb_tasks + 1;
+
+	$data = [];
+	$data['max_priority'] = $max_priority;
+
+    return view('create-task', $data);
+});
+
+Route::post('/create-task', function (Request $request) {
+	$task_data = $request->all();
+	$task = Task::create($task_data);
+
+	update_other_priorities($task->id, Task::count(), $task->priority);
+
+	return redirect('/');
+});
+
+Route::get('/edit-task/{id}', function ($id) {
+	$task = Task::where('id', $id)->first();
+
+	if($task == null) {
+		abort(404);
+	}
+
+	$nb_tasks = Task::count();
+	$max_priority = $nb_tasks;
+
+	$data = [];
+	$data['task'] = $task;
+	$data['max_priority'] = $max_priority;
+
+    return view('edit-task', $data);
+});
+
 
 Route::post('/edit-task/{id}', function ($id, Request $request) {
 
